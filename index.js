487f4d40755d7b64c3ff1b01bca00e9430b85952
gilberto-009199 <gilberto.tec@vivaldi.net>
2023-06-02 23:20:39 -0300

    corrigindo submodules

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ let path = require('path');
 // Dir's Projects
 let dir = path.join(__dirname);
 let dir_audit = path.join(dir,'audit');
+let dir_storage = path.join(dir,'storage');
 let dir_backend = path.join(dir,'backend');
 let dir_common = path.join(dir,'common');
 let dir_frontend = path.join(dir,'frontend');
 let dir_game_machine = path.join(dir,'game_machine');
 
 let audit_exist = fs.existsSync(path.join(dir_audit,'package.json'));
+let storage_exist = fs.existsSync(path.join(dir_storage,'package.json'));
 let backend_exist = fs.existsSync(path.join(dir_backend,'package.json'));
 let common_exist = fs.existsSync(path.join(dir_common,'package.json'));
 let frontend_exist = fs.existsSync(path.join(dir_frontend,'package.json'));
 let game_machine_exist = fs.existsSync(path.join(dir_game_machine,'package.json'));
 
-// verify dir's exist
-if( !audit_exist || !backend_exist || !common_exist || !frontend_exist || !game_machine_exist ){
-    console.log(` [ ${ ('fail'.red) } ] Project not found: `);
+// Verify dir's exist
+if( !audit_exist || !storage_exist || !backend_exist || !common_exist || !frontend_exist || !game_machine_exist ){
+
+    console.log(` [ ${ ('fail'.red) } ] Project's not found: `);
 
     if(!audit_exist)console.log( `\t + ${ (dir_audit.red) }` );
+    
+    if(!audit_exist)console.log( `\t + ${ (dir_storage.red) }` );
 
     if(!backend_exist)console.log( `\t + ${ (dir_backend.red) }` );
 
@@ -35,14 +40,13 @@ if( !audit_exist || !backend_exist || !common_exist || !frontend_exist || !game_
     execSync('git submodule init && git pull --recurse-submodules');
     execSync('git submodule update --remote');
 
-    return;
 }
 
 let file_env = path.join(dir,'.env');
 let file_env_example = path.join(dir,'.env.EXAMPLE');
 
 if(!fs.existsSync(file_env)){
-    console.log(` [ ${ ('fail'.red) } ] Env not found`);
+    console.log(` [ ${ ('Fail'.red) } ] Env not found`);
     fs.copyFileSync(file_env_example, file_env);
     console.log(` [ ${ ('OK'.green) } ] Create Env`);
 }
@@ -50,6 +54,49 @@ if(!fs.existsSync(file_env)){
 let dotenv = require('dotenv');
 dotenv.config({path:'./.env'});
 
+// env file from project's
 
-// env file to project
+let file_env_audit = path.join(dir_audit,'.env');
+let file_env_example_audit = path.join(dir_audit,'.env.EXAMPLE');
+let file_env_storage = path.join(dir_storage,'.env');
+let file_env_example_storage = path.join(dir_storage,'.env.EXAMPLE');
+let file_env_backend = path.join(dir_backend,'.env');
+let file_env_example_backend = path.join(dir_backend,'.env.EXAMPLE');
+let file_env_common = path.join(dir_common,'.env');
+let file_env_example_common = path.join(dir_common,'.env.EXAMPLE');
+let file_env_frontend = path.join(dir_frontend,'.env');
+let file_env_example_frontend = path.join(dir_frontend,'.env.EXAMPLE');
+let file_env_game_machine = path.join(dir_game_machine,'.env');
+let file_env_example_game_machine = path.join(dir_game_machine,'.env.EXAMPLE');
+
+if(!fs.existsSync(file_env_audit)){
+    console.log(` [ ${ ('Fail'.red) } ] Env not found`);
+    fs.copyFileSync(file_env_example_audit, file_env_audit);
+    console.log(` [  ${ ('OK'.green) }  ] Create Env`);
+}
+if(!fs.existsSync(file_env_storage)){
+    console.log(` [ ${ ('Fail'.red) } ] Env not found`);
+    fs.copyFileSync(file_env_example_storage, file_env_storage);
+    console.log(` [  ${ ('OK'.green) }  ] Create Env`);
+}
+if(!fs.existsSync(file_env_backend)){
+    console.log(` [ ${ ('Fail'.red) } ] Env not found`);
+    fs.copyFileSync(file_env_example_backend, file_env_backend);
+    console.log(` [  ${ ('OK'.green) }  ] Create Env`);
+}
+if(!fs.existsSync(file_env_common)){
+    console.log(` [ ${ ('Fail'.red) } ] Env not found`);
+    fs.copyFileSync(file_env_example_common, file_env_common);
+    console.log(` [  ${ ('OK'.green) }  ] Create Env`);
+}
+if(!fs.existsSync(file_env_frontend)){
+    console.log(` [ ${ ('Fail'.red) } ] Env not found`);
+    fs.copyFileSync(file_env_example_frontend, file_env_frontend);
+    console.log(` [  ${ ('OK'.green) }  ] Create Env`);
+}
+if(!fs.existsSync(file_env_game_machine)){
+    console.log(` [ ${ ('Fail'.red) } ] Env not found`);
+    fs.copyFileSync(file_env_example_game_machine, file_env_game_machine);
+    console.log(` [  ${ ('OK'.green) }  ] Create Env`);
+}
 
